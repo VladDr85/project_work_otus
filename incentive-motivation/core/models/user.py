9fce05dc13899fae3f8490fe2eb7398438746703
Bondar_VG <bondar_vg@magnit.ru>
2025-02-02 22:42:24 +0300

    create UserIdType var

--- incentive-motivation/core/models/user.py
+++ incentive-motivation/core/models/user.py
@@ -5,13 +5,14 @@ from fastapi_users.db import (
     SQLAlchemyUserDatabase,
 )
 
+from core.types.user_id import UserIdType
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     pass
 
     @classmethod
