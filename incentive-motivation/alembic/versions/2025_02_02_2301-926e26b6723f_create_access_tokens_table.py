23c860cc5109ffecb9111174f3ce363782ec44c2
Bondar_VG <bondar_vg@magnit.ru>
2025-02-02 23:07:03 +0300

    create access_tokens migrations

---
+++ incentive-motivation/alembic/versions/2025_02_02_2301-926e26b6723f_create_access_tokens_table.py
@@ -0,0 +1,53 @@
+"""create access_tokens table
+
+Revision ID: 926e26b6723f
+Revises: 27a005ce8dd0
+Create Date: 2025-02-02 23:01:38.750666
+
+"""
+
+from typing import Sequence, Union
+
+import fastapi_users_db_sqlalchemy
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "926e26b6723f"
+down_revision: Union[str, None] = "27a005ce8dd0"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "access_tokens",
+        sa.Column("token", sa.String(length=43), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at",
+            fastapi_users_db_sqlalchemy.generics.TIMESTAMPAware(timezone=True),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+            name=op.f("fk_access_tokens_user_id_users"),
+            ondelete="CASCADE",
+        ),
+        sa.PrimaryKeyConstraint("token", name=op.f("pk_access_tokens")),
+    )
+    op.create_index(
+        op.f("ix_access_tokens_created_at"),
+        "access_tokens",
+        ["created_at"],
+        unique=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(
+        op.f("ix_access_tokens_created_at"), table_name="access_tokens"
+    )
+    op.drop_table("access_tokens")
