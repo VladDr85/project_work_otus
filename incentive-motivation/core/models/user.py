4ce305c90a211e9921e7b647cab5bc6c5116c883
Bondar_VG <bondar_vg@magnit.ru>
2025-02-02 22:23:03 +0300

    add UserDatabase

--- incentive-motivation/core/models/user.py
+++ incentive-motivation/core/models/user.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users.db import (
     SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase,
@@ -6,6 +8,12 @@ from fastapi_users.db import (
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
     pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
