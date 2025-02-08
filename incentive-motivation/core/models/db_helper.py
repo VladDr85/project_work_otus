4f1d22914f545e0ec485843902ada6da4a4c3dc8
Bondar_VG <bondar_vg@magnit.ru>
2025-02-08 16:44:27 +0300

    fix access_tokens.py

--- incentive-motivation/core/models/db_helper.py
+++ incentive-motivation/core/models/db_helper.py
@@ -33,12 +33,12 @@ class DatabaseHelper:
             expire_on_commit=False,
         )
 
-        async def dispose(self) -> None:
-            await self.engine.dispose()
+    async def dispose(self) -> None:
+        await self.engine.dispose()
 
-        async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
-            async with self.session_factory() as session:
-                yield session
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
 
 
 db_helper = DatabaseHelper(
