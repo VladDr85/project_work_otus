4e203abef02301bafcf9001b154ed2edfe45c533
Bondar_VG <bondar_vg@magnit.ru>
2025-02-08 16:43:33 +0300

    move fastapi_users

--- incentive-motivation/api/dependencies/authentication/access_tokens.py
+++ incentive-motivation/api/dependencies/authentication/access_tokens.py
@@ -13,10 +13,11 @@ from core.models import (
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
+
 async def get_access_tokens_db(
-        session: Annotated[
-            "AsyncSession",
-            Depends(db_helper.session_getter),
-        ],
+    session: Annotated[
+        "AsyncSession",
+        Depends(db_helper.session_getter),
+    ],
 ):
     yield AccessToken.get_db(session=session)
--- incentive-motivation/core/models/db_helper.py
+++ incentive-motivation/core/models/db_helper.py
@@ -1,19 +1,24 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+)
 
 from core.config import settings
 
 
 class DatabaseHelper:
     def __init__(
-            self,
-            url: str,
-            echo: bool = False,
-            echo_pool: bool = False,
-            pool_size: int = 5,
-            max_overflow: int = 10,
-    ):
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
+    ) -> None:
         self.engine: AsyncEngine = create_async_engine(
             url=url,
             echo=echo,
