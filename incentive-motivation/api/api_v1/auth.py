b77720ad1a2f0e6e0a17741c6ecbc4f679473d0f
Bondar_VG <bondar_vg@magnit.ru>
2025-02-08 23:26:10 +0300

    refactor

--- incentive-motivation/api/api_v1/auth.py
+++ incentive-motivation/api/api_v1/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.dependencies.authentication.backend import authentication_backend
+from api.dependencies.authentication import authentication_backend
 from api.api_v1.fastapi_users import fastapi_users
 from core.config import settings
 from core.schemas.user import (
