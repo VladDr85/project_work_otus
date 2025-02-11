e1749f6938aefe786df98abbcc4430bd4289a3d0
Bondar_VG <bondar_vg@magnit.ru>
2025-02-11 10:48:29 +0300

    add models init

--- incentive-motivation/core/models/__init__.py
+++ incentive-motivation/core/models/__init__.py
@@ -3,9 +3,21 @@ __all__ = (
     "Base",
     "User",
     "AccessToken",
+    "IncentiveList",
+    "Incentive",
+    "Lottery",
+    "Purpose",
+    "Mission",
+    "CompletedMission",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
 from .access_token import AccessToken
+from .incentive_list import IncentiveList
+from .incentive import Incentive
+from .lottery import Lottery
+from .purpose import Purpose
+from .mission import Mission
+from .completed_mission import CompletedMission
